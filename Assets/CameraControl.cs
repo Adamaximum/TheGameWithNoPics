632c6c0cd71c0d974dd05b76ab823a28d98e5ae7
Adam Goren <amg1090@nyu.edu>
2019-09-12 17:14:04 -0400

    Refined jumping and began designing level

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform playerTR;
+    public float cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTR = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTR.position.x+cameraOffset, 0, transform.position.z);
+    }
+}
