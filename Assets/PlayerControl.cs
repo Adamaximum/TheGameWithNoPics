73ec28ed5d1663bdd3cb925c22e533d374c81710
Adam Goren <amg1090@nyu.edu>
2019-09-12 16:08:00 -0400

    Files rearranged; jumping added

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float playerSpeed = 0.2f;
+    float horizontalInput;
+
+    [Range(1,10)]
+    public float jumpVelocity = 2;
+
+    public bool jumpCheck = true;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    public Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalInput = -playerSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalInput = playerSpeed;
+        }
+        else
+        {
+            horizontalInput = 0;
+        }
+
+        transform.position += new Vector3(horizontalInput, 0, 0);
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && jumpCheck == true)
+        {
+            playerRB.velocity = Vector2.up * jumpVelocity;
+        }
+
+        //if (Input.GetButtonDown("Jump") && jumpCheck == true)
+        //{
+        //    playerRB.velocity = Vector2.up * jumpVelocity;
+        //}
+
+        //Control-Pressured Jumping
+        if (playerRB.velocity.y < 0)
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (playerRB.velocity.y > 0 && (!Input.GetKeyDown(KeyCode.W) || !Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            playerRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        //if (playerRB.velocity.y < 0)
+        //{
+        //    playerRB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        //}
+        //else if (playerRB.velocity.y < 0 && !Input.GetButtonDown("Jump"))
+        //{
+        //    playerRB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        //}
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            jumpCheck = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            jumpCheck = false;
+        }
+    }
+}
